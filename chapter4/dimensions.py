08d230a37aea40dc581baf262e0d3a11c5b18069
renzl <renzl@rhel610.(none)>
2019-05-28 17:07:37 +0800

    Chapter4 4#

---
+++ chapter4/dimensions.py
@@ -0,0 +1,10 @@
+print("Original dimensions:")
+dimensions=(200,50)
+#print(dimensions[0])
+#print(dimensions[1])
+for dimension in dimensions:
+ print(dimension)
+print("\nModified dimensions:")
+dimensions=(400,100)
+for dimension in dimensions:
+ print(dimension)
