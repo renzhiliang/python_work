2089f8306ba7395d2444cf7a6db3ac92b67f05aa
renzl <renzl@rhel610.(none)>
2019-05-29 16:24:01 +0800

    Chapter5 1#

---
+++ chapter5/5-2.py
@@ -0,0 +1,18 @@
+print('abc'=='Abc')
+print('abc'=='Abc'.lower())
+print(2==2)
+print(2!=2)
+print(2>2)
+print(2<2)
+print(2>=2)
+print(2<=2)
+print((2>3)and(2<3))
+print((2>3)or(2<3))
+numbers=[1,2,3,4,5,6]
+print(2 in numbers)
+print(7 in numbers)
+
+
+
+
+
---
+++ chapter5/toppings.py
@@ -0,0 +1,2 @@
+requested_topping='mushrooms'
+if requested_topping!='anchovies':print("Hold the anchovies!")
